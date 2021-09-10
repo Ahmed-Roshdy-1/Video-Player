472c655446faea3d21df00b7bf3fd49258f847aa
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-09-11 00:29:58 +0200

    set Progress

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ function displayTime(time) {
   seconds = seconds > 9 ? seconds : `0${seconds}`;
   return `${minutes}:${seconds}`;
 }
+
 // Update Progressbar as video plays
 function updateProgress() {
   progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
@@ -44,8 +45,17 @@ function updateProgress() {
   duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek within the video
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+  console.log(newTime);
+}
+
 // Event Listeners
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
